3d2a4aadf55cf248b3ed5e3a903da81e638dc8be
Don-Chadda <peterturay81@gmail.com>
2022-08-04 16:01:45 +0000

    School_Management_System

---
+++ private/controllers/Students.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * student controller
+ */
+class Students extends Controller
+{
+	
+	function index($id = null)
+	{
+		// code...
+		echo "this is the students controller ". $id;
+	}
+}
